bde032b61b1563f901f5ff98886388aa3132a7b9
Jcatalano67 <Catalanoj1@wit.edu>
2020-11-17 23:39:20 -0500

    GetSolution completed

--- Mastermind/src/edu/wit/comp1050/Code.java
+++ Mastermind/src/edu/wit/comp1050/Code.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 
 public class Code{
-    public static Object[] makeCode(Object[] p) {
+    public static String makeCode(String[] p) {
         // int for random element removal
         ArrayList<Character> colorList = new ArrayList<Character>();
         colorList.add(0,'G');
@@ -15,18 +15,15 @@ public class Code{
        int rand = (int) ((Math.random()* colorList.size()));
        colorList.remove(rand);
        colorList.remove(rand);
-        Object [] solution = colorList.toArray();
-
-
-        return solution;
+        String solutionToString = colorList.toString();
+        return solutionToString;
 
         }
 
     public void getSolution() {
-        Object[] peg = new Object[4];
-        Object [] pegCode = makeCode(peg);
-        System.out.println(Arrays.toString(pegCode));
-        // have list take off too before solution
+        String [] peg = new String[4];
+        String pegCode = makeCode(peg);
+        System.out.println(pegCode);
     }
 
     }
